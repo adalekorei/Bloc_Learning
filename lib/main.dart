6fdab7d2a7c09d1c254ff45583b234c312752dea
Anton Dalekorei <dalekorei.work@gmail.com>
2025-05-18 13:54:25 +0200

    read, select, watch

--- lib/main.dart
+++ lib/main.dart
@@ -21,77 +21,90 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counterBloc = CounterBloc();
-    final userBloc = UserBloc();
     return MultiBlocProvider(
       providers: [
-        BlocProvider<CounterBloc>(create: (context) => counterBloc),
-        BlocProvider<UserBloc>(create: (context) => userBloc),
+        BlocProvider<CounterBloc>(create: (context) => CounterBloc()),
+        BlocProvider<UserBloc>(create: (context) => UserBloc()),
       ],
-      child: Scaffold(
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed: () {
-                counterBloc.add(CounterIncEvent());
-              },
-              icon: Icon(Icons.plus_one),
-            ),
-            IconButton(
-              onPressed: () {
-                counterBloc.add(CounterDecEvent());
-              },
-              icon: Icon(Icons.exposure_minus_1),
-            ),
-            IconButton(
-              onPressed: () {
-                userBloc.add(UserGetUsersEvent(counterBloc.state));
-              },
-              icon: Icon(Icons.person),
-            ),
-            IconButton(
-              onPressed: () {
-                userBloc.add(UserGetUsersJobEvent(counterBloc.state));
-              },
-              icon: Icon(Icons.work),
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Column(
+      child: Builder(
+        builder: (context) {
+          final counterBloc = BlocProvider.of<CounterBloc>(context);
+          return Scaffold(
+            floatingActionButton: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                BlocBuilder<CounterBloc, int>(
-                  bloc: counterBloc,
-                  builder: (context, state) {
-                    return Text(
-                      state.toString(),
-                      style: TextStyle(fontSize: 35),
-                    );
+                IconButton(
+                  onPressed: () {
+                    counterBloc.add(CounterIncEvent());
                   },
+                  icon: Icon(Icons.plus_one),
                 ),
-                BlocBuilder<UserBloc, UserState>(
-                  bloc: userBloc,
-                  builder: (context, state) {
-                    final user = state.users;
-                    final job = state.job;
-                    return Column(
-                      children: [
-                        if (state.isLoading)
-                          CircularProgressIndicator(),
-                        if (user.isNotEmpty)
-                          ...state.users.map((e) => Text(e.name)),
-                          if (job.isNotEmpty)
-                          ...state.job.map((e) => Text(e.name)),
-                      ],
+                IconButton(
+                  onPressed: () {
+                    counterBloc.add(CounterDecEvent());
+                  },
+                  icon: Icon(Icons.exposure_minus_1),
+                ),
+                IconButton(
+                  onPressed: () {
+                    final userBloc = context.read<UserBloc>();
+                    userBloc.add(
+                      UserGetUsersEvent(context.read<CounterBloc>().state),
                     );
                   },
+                  icon: Icon(Icons.person),
+                ),
+                IconButton(
+                  onPressed: () {
+                    final userBloc = context.read<UserBloc>();
+                    userBloc.add(
+                      UserGetUsersJobEvent(context.read<CounterBloc>().state),
+                    );
+                  },
+                  icon: Icon(Icons.work),
                 ),
               ],
             ),
-          ),
-        ),
+            body: SafeArea(
+              child: Center(
+                child: Column(
+                  children: [
+                    BlocBuilder<CounterBloc, int>(
+                      builder: (context, state) {
+                        final users = context.select(
+                          (UserBloc bloc) => bloc.state.users,
+                        );
+                        return Column(
+                          children: [
+                            Text(
+                              state.toString(),
+                              style: TextStyle(fontSize: 35),
+                            ),
+                            if (users.isNotEmpty)
+                              ...users.map((e) => Text(e.name)),
+                          ],
+                        );
+                      },
+                    ),
+                    BlocBuilder<UserBloc, UserState>(
+                      builder: (context, state) {
+                        final user = state.users;
+                        final job = state.job;
+                        return Column(
+                          children: [
+                            if (state.isLoading) CircularProgressIndicator(),
+                            if (job.isNotEmpty)
+                              ...state.job.map((e) => Text(e.name)),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
